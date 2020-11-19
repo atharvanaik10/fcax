68e2c686b4cc0d4f1484ba13b4bf5e79fe6b2be8
atharvanaik <atharvanaik10@gmail.com>
2020-11-19 20:34:22 +0800

    Add more test cases

--- tests/frame_test.cpp
+++ tests/frame_test.cpp
@@ -14,11 +14,40 @@
 using namespace fcax;
 
 TEST_CASE("Test frame extraction") {
-  SECTION("Valid video file") {
+  SECTION("Normal file") {
     FrameExtractor extractor;
-    std::string file = "/Users/atharvanaik/Desktop/opencvtest.MOV";
+    std::string file = "/Users/atharvanaik/CLionProjects/Cinder/my-projects/"
+        "FinalProject/final-project-atharvanaik10/tests/data/93frames.MOV";
     file>>extractor;
     REQUIRE(extractor.GetFrames().size() == 93);
+  }
+  SECTION("Normal file") {
+    FrameExtractor extractor;
+    std::string file = "/Users/atharvanaik/CLionProjects/Cinder/my-projects/"
+        "FinalProject/final-project-atharvanaik10/tests/data/28frames.mp4";
+    file>>extractor;
+    REQUIRE(extractor.GetFrames().size() == 28);
+  }
+  SECTION("Normal file mp4") {
+    FrameExtractor extractor;
+    std::string file = "/Users/atharvanaik/CLionProjects/Cinder/my-projects/"
+        "FinalProject/final-project-atharvanaik10/tests/data/95frames.mp4";
+    file>>extractor;
+    REQUIRE(extractor.GetFrames().size() == 95);
+  }
+  SECTION("1 frame mp4") {
+    FrameExtractor extractor;
+    std::string file = "/Users/atharvanaik/CLionProjects/Cinder/my-projects/"
+        "FinalProject/final-project-atharvanaik10/tests/data/1frame.mp4";
+    file>>extractor;
+    REQUIRE(extractor.GetFrames().size() == 1);
+  }
+  SECTION("Large file") {
+    FrameExtractor extractor;
+    std::string file = "/Users/atharvanaik/CLionProjects/Cinder/my-projects/"
+        "FinalProject/final-project-atharvanaik10/tests/data/17frames.MOV";
+    file>>extractor;
+    REQUIRE(extractor.GetFrames().size() == 17);
   }
 }
 
